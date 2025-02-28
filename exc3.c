8858988ea5f5f8bb68cb326841326e03d50aa2f4
minhisnothere <AT210335@actvn.edu.vn>
2025-02-28 10:57:32 +0700

    exc3.c

---
+++ exc3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int x,y,a,b,c,d;
+	scanf("%d%d",&x,&y);
+	printf("%d\n",x+y);
+	printf("%d\n", x-y);
+	printf ("%d\n", x*y);
+	printf("%.3f", (float)x/y);
+}
