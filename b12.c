1224fa15b039c141d21cfef58c67ad96d9d903c1
minhisnothere <AT210335@actvn.edu.vn>
2025-03-05 20:50:40 +0700

    Create b12.c

---
+++ b12.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main (){
+	int j=1,i=0, giaithua=1;
+	double tong=0;
+	float e, x;
+	printf("nhap vao so x ");
+	scanf("%f",&x);
+	printf("nhap vao so eps ");
+	scanf("%f", &e);
+	x=x*3.14/180; 
+	float o=x;
+	while (fabs(o)>e){
+		o=(pow(x,j)/giaithua)*pow(-1,i);
+		tong+=o;
+		giaithua=giaithua*(j+2)*(j+1);
+		j+=2;
+		i+=1;
+	}
+	printf("%lf",tong);
+}
